254d901a6a3d4630645a04000ce5fd05a236ae67
Pranabesh Paul <p.pranb2009@gmail.com>
2024-01-26 22:26:25 -0700

    Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
    Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
    
    Example 1:
    
    Input: s = "abciiidef", k = 3
    Output: 3
    Explanation: The substring "iii" contains 3 vowel letters.
    Example 2:
    
    Input: s = "aeiou", k = 2
    Output: 2
    Explanation: Any substring of length 2 contains 2 vowels.
    Example 3:
    
    Input: s = "leetcode", k = 3
    Output: 2
    Explanation: "lee", "eet" and "ode" contain 2 vowels.

---
+++ src/main/java/com/example/algorithm/string/MaxVowelInSubString.java
@@ -0,0 +1,64 @@
+package com.example.algorithm.string;
+
+/**
+ * Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+ *
+ * Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "abciiidef", k = 3
+ * Output: 3
+ * Explanation: The substring "iii" contains 3 vowel letters.
+ * Example 2:
+ *
+ * Input: s = "aeiou", k = 2
+ * Output: 2
+ * Explanation: Any substring of length 2 contains 2 vowels.
+ * Example 3:
+ *
+ * Input: s = "leetcode", k = 3
+ * Output: 2
+ * Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ */
+public class MaxVowelInSubString {
+    public static int maxVowels(String s, int k){
+        int total = 0;
+        int max = 0;
+        if(s ==null || s.isBlank()){
+            return 0;
+        }
+        if(s.length()<=k){
+            for(int i = 0 ; i< s.length() ; i++){
+                if(isVowel(s.charAt(i))){
+                    total++;
+                }
+            }
+            return total;
+        }
+
+        for(int i = 0 ; i< k ; i++){
+            if(isVowel(s.charAt(i))){
+                total++;
+            }
+        }
+        max = total;
+        for(int i = k ; i< s.length() ; i++){
+            if(isVowel(s.charAt(i-k))){
+                total--;
+            }
+            if(isVowel(s.charAt(i))){
+                total++;
+            }
+            if(max<total){
+                max=total;
+            }
+        }
+        return max;
+    }
+    private static boolean isVowel(char aChar){
+        return aChar =='a'||aChar=='e'||aChar=='i'||aChar=='o'||aChar=='u';
+    }
+}
